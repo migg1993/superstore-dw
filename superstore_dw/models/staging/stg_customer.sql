9c94233b47d5aa24e67429cd71eb42e0d6095698
Gonçalo Nogueira <jgoncalonogueira@gmail.com>
2024-03-27 10:05:12 +0000

    Delete stg_customer

--- superstore_dw/models/staging/stg_customer.sql
+++
@@ -1,6 +0,0 @@
-select
-    c.customer_id,
-    c.name as customer_name,
-    s.segment
-from {{ source("norm", "t_customer") }} c
-    join {{ source("norm", "t_segment") }} s on c.segment_id = s.segment_id
